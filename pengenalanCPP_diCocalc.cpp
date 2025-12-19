bbece03d4e7d9bf3b024aebdbd62d1f93128c8ea
zalikasetiawan07-maker <zalikasetiawan07@gmail.com>
2025-12-19 22:17:21 +0700

    new

---
+++ pengenalanCPP_diCocalc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string nama = "--Zalika Alhena Dinar Setiawan--";
+  string NPM = "--2517051007--";
+
+  cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+  cout << "Nama saya adalah" << nama << endl;
+  cout << "NPM saya adalah" << NPM << endl << endl;
+
+  cout << "Unila Indah" << endl;
+  cout << "Be Strong" << endl;
+
+  return 0;
+}
---
+++ tugasDiCocalc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string naleng = "_Zalika Alhena Dinar Setiawan_";
+  string napang = "_Zalika_";
+  string umur = "_18 tahun_";
+  string NPM = "_2517051007_";
+  string FJP = "_Fakultas MIPA, Jurusan Ilmu Kossmputer, Prodi S1 Ilmu Komputer_";
+  string daerah = "_Bandar Lampung_";
+
+  cout << "Halooo Kakak-Kakak Dan Teman-Teman Jurusan Ilmu Komputer" << endl;
+  cout << "Nama saya adalah" << naleng << endl;
+  cout << "Saya biasa dipanggil" << napang << endl;
+  cout << "Umur saya di tahun ini" << umur << endl;
+  cout << "NPM saya adalah" << NPM << endl;
+  cout << "Saya dari" << FJP << endl;
+  cout << "Saya berasal dari daerah" << daerah << endl << endl;
+
+  cout << "SEMANGAT TEMAN-TEMAN DAN KAKAK-KAKAK!!!" << endl;
+
+  return 0;
+}
